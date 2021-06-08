4f9f3ec9938f958f841e6843dc8b5b3c251d010b
akash0258 <84716437+akash0258@users.noreply.github.com>
2021-06-08 16:38:59 +0530

    Add files via upload

---
+++ Diabetes.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
+diabetes = pd.read_csv('diabetes.csv')
+print(diabetes.columns)
+
+print("dimension of diabetes data: {}".format(diabetes.shape))
+
+print(diabetes.groupby('Outcome').size())
+
+import seaborn as sns
+sns.countplot(diabetes['Outcome'],label="Count")
+
+diabetes.info()
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(diabetes.loc[:, diabetes.columns != 'Outcome'], diabetes['Outcome'], stratify=diabetes['Outcome'], random_state=66)
+from sklearn.neighbors import KNeighborsClassifier
+training_accuracy = []
+test_accuracy = []
+# try n_neighbors from 1 to 10
+neighbors_settings = range(1, 11)
+for n_neighbors in neighbors_settings:
+    # build the model
+    knn = KNeighborsClassifier(n_neighbors=n_neighbors)
+    knn.fit(X_train, y_train)
+    # record training set accuracy
+    training_accuracy.append(knn.score(X_train, y_train))
+    # record test set accuracy
+    test_accuracy.append(knn.score(X_test, y_test))
+plt.plot(neighbors_settings, training_accuracy, label="training accuracy")
+plt.plot(neighbors_settings, test_accuracy, label="test accuracy")
+plt.ylabel("Accuracy")
+plt.xlabel("n_neighbors")
+plt.legend()
+plt.savefig('knn_compare_model')
+
+knn = KNeighborsClassifier(n_neighbors=9)
+knn.fit(X_train, y_train)
+print('Accuracy of K-NN classifier on training set: {:.2f}'.format(knn.score(X_train, y_train)))
+print('Accuracy of K-NN classifier on test set: {:.2f}'.format(knn.score(X_test, y_test)))
+
+from sklearn.tree import DecisionTreeClassifier
+tree = DecisionTreeClassifier(random_state=0)
+tree.fit(X_train, y_train)
+print("Accuracy on training set: {:.3f}".format(tree.score(X_train, y_train)))
+print("Accuracy on test set: {:.3f}".format(tree.score(X_test, y_test)))
+
+tree = DecisionTreeClassifier(max_depth=3, random_state=0)
+tree.fit(X_train, y_train)
+print("Accuracy on training set: {:.3f}".format(tree.score(X_train, y_train)))
+print("Accuracy on test set: {:.3f}".format(tree.score(X_test, y_test)))
+
+print("Feature importances:\n{}".format(tree.feature_importances_))
+
+def plot_feature_importances_diabetes(model):
+    plt.figure(figsize=(8,6))
+    n_features = 8
+    plt.barh(range(n_features), model.feature_importances_, align='center')
+    plt.yticks(np.arange(n_features), diabetes_features)
+    plt.xlabel("Feature importance")
+    plt.ylabel("Feature")
+    plt.ylim(-1, n_features)
+plot_feature_importances_diabetes(tree)
+plt.savefig('feature_importance')
+
+from sklearn.ensemble import RandomForestClassifier
+rf = RandomForestClassifier(n_estimators=100, random_state=0)
+rf.fit(X_train, y_train)
+print("Accuracy on training set: {:.3f}".format(rf.score(X_train, y_train)))
+print("Accuracy on test set: {:.3f}".format(rf.score(X_test, y_test)))
+
+rf1 = RandomForestClassifier(max_depth=3, n_estimators=100, random_state=0)
+rf1.fit(X_train, y_train)
+print("Accuracy on training set: {:.3f}".format(rf1.score(X_train, y_train)))
+print("Accuracy on test set: {:.3f}".format(rf1.score(X_test, y_test)))
+
+plot_feature_importances_diabetes(rf)
+
